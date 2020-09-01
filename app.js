4f930a6b9090304cf90bce1a55ccffd14285fc7b
jdittrich <d_jan@ymail.com>
2020-09-01 21:21:06 +0200

    now composition API use of document data

--- app.js
+++ app.js
@@ -1,10 +1,59 @@
 import documentElement from './documentElement.js   '
+import { ref, reactive } from './vue.esm-browser.js'
 
+function useDocumentElements(){
+    let documentElements = reactive([
+        {
+            width: 100,
+            height: 130,
+            pos_x: 10,
+            pos_y: 20,
+            id: 1
+        },
+        {
+            width: 30,
+            height: 40,
+            pos_x: 200,
+            pos_y: 30,
+            id: 2
+        },
+        {
+            width: 140,
+            height: 50,
+            pos_x: 200,
+            pos_y: 40,
+            id: 3
+        }
+    ]);
+    
+    function getDocumentElementById(id){
+        const elementToGet = documentElements.find(element => element.id === id)
+        return elementToGet;
+    };
+
+    //function to move elements
+    //function to end move
+    function moveElementBy(id,pos_diff){
+        const {pos_x_diff, pos_y_diff} = pos_diff;
+        const elementToMove = getDocumentElementById(id);
+        elementToMove.pos_x += pos_x_diff;
+        elementToMove.pos_y += pos_y_diff;
+    }
+
+    return {
+        documentElements,
+        moveElementBy
+    }
+}
 
 export default {
     name:'app',
     components:{
         'document-element':documentElement
+    },
+    setup(props){
+        const {documentElements, moveElementBy} = useDocumentElements();
+        return {documentElements, moveElementBy}
     },
     data:function(){
         return {
@@ -20,30 +69,7 @@ export default {
                 clientpos_y:null
             },
             isdragging:false,
-            selectedElement:null,
-            documentElements:[
-                {
-                    width:100,
-                    height:130,
-                    pos_x: 10,
-                    pos_y: 20,
-                    id: 1
-                },
-                {
-                    width:30,
-                    height:40,
-                    pos_x: 200,
-                    pos_y: 30,
-                    id: 2
-                },
-                {
-                    width:140,
-                    height:50,
-                    pos_x: 200,
-                    pos_y: 40,
-                    id: 3
-                }
-            ]
+            selectedElement:null
         };   
     },
     methods:{
@@ -58,15 +84,15 @@ export default {
             if(this.isdragging){
                 const pos_x_diff = event.movementX;
                 const pos_y_diff = event.movementY;
-                this.selectedElement.pos_x += pos_x_diff;
-                this.selectedElement.pos_y += pos_y_diff; 
+
+                this.moveElementBy(this.selectedElement, { pos_x_diff, pos_y_diff})
             }
         },
         mouseup(event){
             this.isdragging = false; 
         },
         setMousedown(event,rectSpec){
-            this.selectedElement = rectSpec;
+            this.selectedElement = rectSpec.id;
         }
     },
     template:`
