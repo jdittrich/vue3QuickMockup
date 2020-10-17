0bb761a8f7411d65a8f6854f561d8308f01b48f6
jdittrich <d_jan@ymail.com>
2020-10-17 15:43:37 +0200

    Not yet working: helpers for converting nested to global position.

--- useDocumentElements.js
+++ useDocumentElements.js
@@ -1,57 +1,107 @@
 import {reactive, ref, computed, watch} from './vue.esm-browser.js'
 
-let documentElements = reactive([
-    {
-        width: 100,
-        height: 130,
-        pos_x: 10,
-        pos_y: 20,
-        id: "1",
-        children:[{
+// should I keep it array or better have a root node? Probably a root node. I can leave the root note pretty much empty. 
+// when I use it as canvas, I can add stuff like zoom and width and whatnot somewhere else; the <qm-canvas> or whatever does only
+// need to access child objects (in HTML the root node is the same as the .document)
+let documentElements = reactive({
+    id: "documentElementsRootNode",
+    children: [{
+            width: 100,
+            height: 130,
+            pos_x: 10,
+            pos_y: 20,
+            id: "1",
+            children: [{
+                width: 30,
+                height: 40,
+                pos_x: 10,
+                pos_y: 10,
+                children: [],
+                id: "99"
+            }]
+        },
+        {
             width: 30,
             height: 40,
-            pos_x: 10,
-            pos_y: 10,
+            pos_x: 200,
+            pos_y: 30,
             children: [],
-            id: "99"
-            }
-        ]
-    },
-    {
-        width: 30,
-        height: 40,
-        pos_x: 200,
-        pos_y: 30,
-        children: [],
-        id: "2"
-    },
-    {
-        width: 140,
-        height: 50,
-        pos_x: 200,
-        pos_y: 40,
-        children: [],
-        id: "3"
-    }
-]);
+            id: "2"
+        },
+        {
+            width: 140,
+            height: 50,
+            pos_x: 200,
+            pos_y: 40,
+            children: [],
+            id: "3"
+        }
+    ]
+});
 
 const selectedElementId = ref("null");
 
 
 
 
-
-
 function useDocumentElements() {
     // HELPERS
-    function _getDocumentElementById(idToFind) {
-        //wrap the document in an empty root node
-        const rootNodeTree= {
-            "children": documentElements,
-            "id": Symbol("rootNode") //so you CANT pass anything that returns you this root node
+    function _calculatePositionOnCanvas(elementId,documentElements){
+           
+        const flatDocumentData = _getFlatDocumentData(documentElements);
+            
+        function getParentOf(flatDocumentData, idToSeachFor){
+            const parent = flatDocumentData.find(element => element.children.includes(idToSeachFor));
+            return parent; 
         }
 
-        const elementToGet = searchTree(rootNodeTree, idToFind)
+        // create an array containing all the parents 
+        let idToSeachFor = elementId;
+        let currentElement = _getDocumentElementById(elementId)
+        let parentChain = [];
+
+        while (currentElement && idToSearchFor !== "documentElementsRootNode"){
+            
+            parentChain.push(currentElement)
+
+            currentElement = getParentOf(flatDocumentData, idToSeachFor);
+            idToSearchFor = currentElement.id;
+        }
+        
+        //now add all positions along the parent chain. 
+        const offset = parentChain.reduce((accumulator, currentValue)=>{
+            return {
+                pos_x: accumulator.pos_x + currentValue.pos_x,
+                pos_y: accumulator.pos_y + currentValue.pos_y
+            }
+        })
+
+        return offset
+    }
+
+    function _getFlatDocumentData(documentElementTree) {
+        let flatDocumentData = [];
+
+        flatten(documentElementTree);
+
+        function flatten(documentElementTree) {
+            const toAppend = Object.assign({}, documentElementTree, {
+                children: documentElementTree.children.map(element => element.id)
+            });
+            flatDocumentData.push(toAppend);
+            
+            if (documentElementTree.children.length > 0) {
+                documentElementTree.children.forEach(element => flatten(element))
+            }
+        }
+
+        return flatDocumentData;
+    }
+
+
+    function _getDocumentElementById(documentElementTree, idToFind) {
+
+        const elementToGet = searchTree(documentElementTree, idToFind)
 
         // https://stackoverflow.com/questions/9133680#9133680, CC BY-SA_3, driangle, tanman
         function searchTree(element, idToFind) {
@@ -80,7 +130,7 @@ function useDocumentElements() {
 
     function moveElementBy(id, pos_diff) {
         const { pos_x_diff, pos_y_diff } = pos_diff;
-        const elementToMove = _getDocumentElementById(id);
+        const elementToMove = _getDocumentElementById(documentElements,id);
         elementToMove.pos_x += pos_x_diff;
         elementToMove.pos_y += pos_y_diff;
     }
@@ -93,7 +143,7 @@ function useDocumentElements() {
     
     function resizeElementBy(id,pos_diff,sides){     
         const { pos_x_diff, pos_y_diff } = pos_diff;
-        const elementToResize = _getDocumentElementById(id);
+        const elementToResize = _getDocumentElementById(documentElements,id);
 
         //if top is selected
         if(sides.top===true){
@@ -136,7 +186,14 @@ function useDocumentElements() {
         selectedElementId.value = null
     }
 
-    const selectedElement = computed(()=>_getDocumentElementById(selectedElementId.value));
+    const selectedElement = computed(()=>{
+        const selectedElementData = _getDocumentElementById(documentElements, selectedElementId.value)
+        return selectedElementData;
+        
+        // const positionOnCanvas = 
+        //  selectedElement
+        // _getDocumentElementById(selectedElementId.value)
+    });
     // A note on learning: This only got "activated" aka seen-as-computed when I used .value
     // I tried quite some other things like using watch instead or tried to track the value 
     // using this.selectedElement in the template etc. But .value it was.
