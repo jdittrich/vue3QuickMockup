5a7d2223383747fc7fe944feb2d2235630c193cd
jdittrich <d_jan@ymail.com>
2020-10-19 00:13:30 +0200

    fix: did not download dependencies

--- downloadDependencies.sh
+++ downloadDependencies.sh
@@ -4,7 +4,6 @@ echo 'This will download vue.esm-browser.js to ./ (this directory)'
 echo 'and chai.js mocha.js mocha.css  to ./tests'
 
 curl -o vue.esm-browser.js https://unpkg.com/vue@3.0.0/dist/vue.esm-browser.js
-curl -o ./tests/chai.js  https://unpkg.com/chai/chai.js
-curl -o ./tests/mocha.js https://unpkg.com/mocha/mocha.js
-curl -o ./tests/mocha.css https://unpkg.com/mocha/mocha.css
-
+curl -o ./tests/chai.js  https://unpkg.com/chai@4.2.0/chai.js
+curl -o ./tests/mocha.js https://unpkg.com/mocha@8.2.0/mocha.js
+curl -o ./tests/mocha.css https://unpkg.com/mocha@8.2.0/mocha.css 
