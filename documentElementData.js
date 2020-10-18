004dcecac4d9c56441f11607111e188487434631
jdittrich <d_jan@ymail.com>
2020-10-18 15:04:26 +0200

    extracted plain data

---
+++ documentElementData.js
@@ -0,0 +1,35 @@
+export default{
+    id: "documentElementsRootNode",
+    children: [{
+            width: 100,
+            height: 130,
+            pos_x: 10,
+            pos_y: 20,
+            id: "1",
+            children: [{
+                width: 30,
+                height: 40,
+                pos_x: 10,
+                pos_y: 10,
+                children: [],
+                id: "99"
+            }]
+        },
+        {
+            width: 30,
+            height: 40,
+            pos_x: 200,
+            pos_y: 30,
+            children: [],
+            id: "2"
+        },
+        {
+            width: 140,
+            height: 50,
+            pos_x: 200,
+            pos_y: 40,
+            children: [],
+            id: "3"
+        }
+    ]
+}
